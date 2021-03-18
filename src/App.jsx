1aeab289f380e806c18587b912f6cbd4c73c74bb
RogerioRomualdo <rogerioromualdo12@gmail.com>
2021-03-18 02:16:14 -0300

    Created the visual part of the login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import "./App.css";
-import React from "react";
-import {Route, BrowserRouter as Router} from "react-router-dom";
-import Login from "./pages/login/login";
-import List from "./pages/list/list";
-import Inspect from "./pages/inspect/inspect";
-import Add from "./pages/add/add";
+import './App.css';
+import React from 'react';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import Login from './pages/login/login';
+import List from './pages/list/list';
+import Inspect from './pages/inspect/inspect';
+import Add from './pages/add/add';
 
 export default (props) => {
-    return(
-        <Router>
-            <div className="App">
-            </div>
-            <Route path="/" exact component={Login}/>
-            <Route path="/list" exact component={List}/>
-            <Route path="/inspect" exact component={Inspect}/>
-            <Route path="/add" exact component={Add}/>
-        </Router>
-    )
-}
-
+  return (
+    <Router>
+      <div className="App"></div>
+      <Route path="/" exact component={Login} />
+      <Route path="/list" exact component={List} />
+      <Route path="/inspect" exact component={Inspect} />
+      <Route path="/add" exact component={Add} />
+    </Router>
+  );
+};
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './login.css';
+import eye from '../../img/eye.png';
 
-export default props =>{
-    return(
-        <div className="Login">This is the login page</div>
-    )
-}
+export default (props) => {
+  const [varType, setVarType] = useState('password');
+
+  return (
+    <div className="Container">
+      <div className="welcomeText">
+        <div className="upperText">
+          <h1>Seja bem vindo</h1>
+          <h2>Para continuar por favor faça login</h2>
+        </div>
+        <div className="lowerText">
+          <h3>Logotipo</h3>
+        </div>
+      </div>
+      <div className="loginBox">
+        <div className="boxContainer">
+          <h1>Utilize o painel agora mesmo</h1>
+          <input placeholder="Digite seu email"></input>
+          <div className="centerInput">
+            <div className="password">
+              <input type={varType} placeholder="Digite sua senha"></input>
+              <img
+                src={eye}
+                className="eye"
+                onClick={() => {
+                  varType === 'text' ? setVarType('password') : setVarType('text');
+                }}
+              />
+            </div>
+          </div>
+          <h3>Esqueci minha senha</h3>
+          <button className="submit">Entrar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
